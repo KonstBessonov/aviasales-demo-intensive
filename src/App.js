cd0d430607d2350ba0191b2302e2898129f4e895
Konstantin Bessonov <bessonov.ka@gmail.com>
2018-02-13 12:47:56 +0500

    Section BlueHeader renamed to MainForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import BlueHeader from "./BlueHeader";
+import MainForm from "./MainForm";
 import TopDestination from "./TopDestination";
 import PopularDirections from "./PopularDirections";
 import SpecialOffers from "./SpecialOffers";
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BlueHeader />
+        <MainForm />
         <TopDestination />
         <PopularDirections />
         <SpecialOffers />
