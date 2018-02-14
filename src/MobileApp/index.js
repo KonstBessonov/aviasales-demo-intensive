f1cf93366b861d29ca69869a7dc198d1bdb1c68e
Konstantin Bessonov <bessonov.ka@gmail.com>
2018-02-14 11:06:23 +0500

    MobileApp section stage 2

--- src/MobileApp/index.js
+++ src/MobileApp/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { BlueSection } from "../assets";
+import rating from "./rating.svg";
+import phone_sm from "./phone_sm.png";
+
+import apple from "./apple.svg";
+import android from "./android.svg";
+import winphone from "./winphone.svg";
+
+const MobileApp = BlueSection.extend`
+  padding-bottom: 0;
+`;
 
 const Title = styled.h2`
   font-size: 3rem;
@@ -9,9 +19,40 @@ const Title = styled.h2`
   text-align: center;
 `;
 
+const Rating = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const App = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const AppList = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 3rem;
+`;
+
+const AppLink = styled.a`
+  color: inherit;
+  text-decoration: none;
+  margin: 1.5rem 0;
+  &:hover {
+    text-decoration: underline;
+  }
+  &:before {
+    position: relative;
+    right: 1rem;
+    content: url(${props => props.img});
+  }
+`;
+
 export default () => {
   return (
-    <BlueSection>
+    <MobileApp>
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-lg-10 col-lg-offset-1">
@@ -19,6 +60,25 @@ export default () => {
           </div>
         </div>
       </div>
-    </BlueSection>
+      <Rating>
+        <img src={rating} alt="rating 4.6 stars" /> Более 103&nbsp;000 оценок
+      </Rating>
+      <App>
+        <div>
+          <img src={phone_sm} alt="" />
+        </div>
+        <AppList>
+          <AppLink href="#" img={apple}>
+            iPhone или iPad
+          </AppLink>
+          <AppLink href="#" img={android}>
+            Android
+          </AppLink>
+          <AppLink href="#" img={winphone}>
+            Windows Phone
+          </AppLink>
+        </AppList>
+      </App>
+    </MobileApp>
   );
 };
