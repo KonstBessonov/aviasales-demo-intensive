9e46582a50a0dd0c6242854b7d518536b3ea56a2
Konstantin Bessonov <bessonov.ka@gmail.com>
2018-02-25 15:12:46 +0500

    Fake data collect

--- src/Forms/Search/data.js
+++ src/Forms/Search/data.js
@@ -9,53 +9,43 @@ import redw_sm from "./images/redw_sm.png";
 import some_sm from "./images/some_sm.png";
 
 export const results = [
-  // {
-  //   price: 7712,
-  //   handLuggage: 5,
-  //   baggage: 15,
-  //   partner: "Cliackavia",
-  //   airline: russia,
-  //   isCharter: true,
-  //   forward: {
-  //     departure_time: {
-  //       datetime: Date(2018, 1, 24, 0, 5),
-  //       city: "Москва",
-  //       airport: "VKO"
-  //     },
-  //     arrival_time: {
-  //       datetime: Date(2018, 1, 24, 3, 5),
-  //       city: "Барселона",
-  //       airport: "BCN"
-  //     },
-  //     duration: 300,
-  //     transfers: []
-  //   },
-  //   reverse: {
-  //     departure_time: {
-  //       datetime: Date(2018, 2, 3, 10, 35),
-  //       city: "Барселона",
-  //       airport: "BCN"
-  //     },
-  //     arrival_time: {
-  //       datetime: Date(2018, 2, 3, 17, 10),
-  //       city: "Москва",
-  //       airport: "VKO"
-  //     },
-  //     duration: 275,
-  //     transfers: 0
-  //   }
-  // },
+  {
+    price: 7712,
+    airline: russia,
+    badges: { isCharter: true },
+    partner: "Cliackavia",
+    handbags: 5,
+    baggage: "1PC15",
+    segments: [
+      {
+        segmentAirports: ["VKO", "BCN"],
+        departure_time: "00:05",
+        departure_date: "2018-02-24",
+        arrival_time: "03:05",
+        arrival_date: "2018-02-24",
+        duration: 300
+      },
+      {
+        segmentAirports: ["BCN", "SVO"],
+        departure_time: "10:35",
+        arrival_time: "17:10",
+        departure_date: "2018-03-03",
+        arrival_date: "2018-03-03",
+        duration: 275
+      }
+    ]
+  },
   {
     price: 9269,
     ticketsRemain: 4,
     airline: fly,
     badges: { isCharter: true },
     partner: "Biletix",
-    handLuggage: undefined,
+    handbags: undefined,
     baggage: "1PC20",
     segments: [
       {
-        segmentAirports: ["MOW", "BCN"],
+        segmentAirports: ["VKO", "BCN"],
         departure_time: "07:30",
         departure_date: "2018-02-24",
         arrival_time: "09:50",
@@ -63,7 +53,7 @@ export const results = [
         duration: 260
       },
       {
-        segmentAirports: ["BCN", "CDG", "MOW"],
+        segmentAirports: ["BCN", "CDG", "SVO"],
         departure_time: "11:20",
         arrival_time: "17:35",
         departure_date: "2018-03-03",
@@ -84,9 +74,12 @@ export const results = [
     price: 8029,
     airline: nordw,
     badges: {},
+    partner: "Билетик Аэро",
+    handbags: 5,
+    baggage: "",
     segments: [
       {
-        segmentAirports: ["MOW", "BCN"],
+        segmentAirports: ["VKO", "BCN"],
         departure_time: "00:15",
         departure_date: "2018-02-24",
         arrival_time: "03:10",
@@ -94,7 +87,7 @@ export const results = [
         duration: 295
       },
       {
-        segmentAirports: ["BCN", "MOW"],
+        segmentAirports: ["BCN", "SVO"],
         departure_time: "10:45",
         arrival_time: "17:15",
         departure_date: "2018-03-03",
@@ -107,9 +100,12 @@ export const results = [
     price: 8164,
     airline: nordw2,
     badges: { isCharter: true },
+    partner: "Авиакасса",
+    handbags: 5,
+    baggage: "",
     segments: [
       {
-        segmentAirports: ["MOW", "BCN"],
+        segmentAirports: ["VKO", "BCN"],
         departure_time: "00:15",
         departure_date: "2018-02-24",
         arrival_time: "03:10",
@@ -117,7 +113,7 @@ export const results = [
         duration: 295
       },
       {
-        segmentAirports: ["BCN", "MOW"],
+        segmentAirports: ["BCN", "SVO"],
         departure_time: "10:45",
         arrival_time: "17:15",
         departure_date: "2018-03-03",
@@ -130,9 +126,12 @@ export const results = [
     price: 8240,
     airline: redw,
     badges: { isCharter: true },
+    partner: "Билетик Аэро",
+    handbags: 10,
+    baggage: "",
     segments: [
       {
-        segmentAirports: ["MOW", "BCN"],
+        segmentAirports: ["VKO", "BCN"],
         departure_time: "00:15",
         departure_date: "2018-02-24",
         arrival_time: "03:10",
@@ -140,22 +139,32 @@ export const results = [
         duration: 295
       },
       {
-        segmentAirports: ["BCN", "MOW"],
+        segmentAirports: ["BCN", "SVO"],
         departure_time: "10:45",
         arrival_time: "17:15",
         departure_date: "2018-03-03",
         arrival_date: "2018-03-03",
         duration: 270
       }
+    ],
+    others: [
+      { partner: "Svyaznoy Travel", price: 9275 },
+      { partner: "Билетик Аэро", price: 9587 },
+      { partner: "Other 1", price: 10000 },
+      { partner: "Other 2", price: 11000 },
+      { partner: "Other 3", price: 12000 }
     ]
   },
   {
     price: 9108,
     airline: [redw_sm, russia_sm],
     badges: { isCharter: true },
+    partner: "Clickavia",
+    handbags: 10,
+    baggage: "1PC15",
     segments: [
       {
-        segmentAirports: ["MOW", "BCN"],
+        segmentAirports: ["VKO", "BCN"],
         departure_time: "00:15",
         departure_date: "2018-02-24",
         arrival_time: "03:10",
@@ -163,7 +172,7 @@ export const results = [
         duration: 295
       },
       {
-        segmentAirports: ["BCN", "MOW"],
+        segmentAirports: ["BCN", "SVO"],
         departure_time: "10:45",
         arrival_time: "17:15",
         departure_date: "2018-03-03",
@@ -176,9 +185,12 @@ export const results = [
     price: 9485,
     airline: [russia_sm, some_sm],
     badges: { isCharter: true },
+    partner: "Clickavia",
+    handbags: undefined,
+    baggage: "1PC15",
     segments: [
       {
-        segmentAirports: ["MOW", "BCN"],
+        segmentAirports: ["VKO", "BCN"],
         departure_time: "00:15",
         departure_date: "2018-02-24",
         arrival_time: "03:10",
@@ -186,7 +198,7 @@ export const results = [
         duration: 295
       },
       {
-        segmentAirports: ["BCN", "MOW"],
+        segmentAirports: ["BCN", "SVO"],
         departure_time: "10:45",
         arrival_time: "17:15",
         departure_date: "2018-03-03",
